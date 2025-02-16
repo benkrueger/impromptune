46e509ddbf495257c7520cafbb55f25f87f69e92
bejamin krueger <benjamin.oliver.krueger@gmail.com>
2025-02-16 14:27:05 -0600

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './public/app.js', // Change this to your main JS file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  mode: 'production', // or 'development', depending on your needs
+  // other configurations...
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
